b5db5fc0561f0b3d2715508d9b07964a339c486b
Lais Schiavone Sobral <202304133441@alunos.estacio.br>
2023-09-13 09:17:18 -0700

    Exercicio Completo

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿string Nome, Sobrenome;
+
+Console.Write("Escreva seu Primeiro Nome:");
+Nome=Console.ReadLine();
+
+Console.Write("Escreva seu Sobrenome:");
+Sobrenome=Console.ReadLine();
+
+Console.WriteLine($"{Nome} {Sobrenome}");
+Console.WriteLine($"{Sobrenome.ToUpper()}, {Nome}");
